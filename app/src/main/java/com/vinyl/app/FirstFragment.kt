82b2bfe2cc15f289daa08906b1603a13aa1a5504
ja-ardila <ja.ardila2@uniandes.edu.co>
2024-04-24 23:08:39 -0500

    Cambios para obtener Artistas con API. La URL toca cambiarla debido a que no funciona ese despliegue

--- app/src/main/java/com/vinyl/app/FirstFragment.kt
+++ app/src/main/java/com/vinyl/app/FirstFragment.kt
@@ -32,9 +32,9 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        /*binding.fetchArtists.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        }*/
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/vinyl/app/MainActivity.kt
+++ app/src/main/java/com/vinyl/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.vinyl.app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,29 +11,55 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.vinyl.app.databinding.ActivityMainBinding
+import android.content.Intent
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import com.android.volley.Response
+import com.vinyl.app.brokers.VolleyBroker
+import com.google.android.material.textfield.TextInputEditText
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    lateinit var volleyBroker: VolleyBroker
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        /*binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.toolbar)*/
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        //val navController = findNavController(R.id.nav_host_fragment_content_main)
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        //setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
+        volleyBroker = VolleyBroker(this.applicationContext)
+
+        val getButton: Button = findViewById(R.id.fetch_artists)
+        val getResultTextView : TextView = findViewById(R.id.get_artists)
+        getButton.setOnClickListener {
+            volleyBroker.instance.add(VolleyBroker.getRequest("artists",
+                { response ->
+                    // Display the first 500 characters of the response string.
+                    getResultTextView.text = "Response is: $response"
+                },
+                {
+                    Log.d("TAG", it.toString())
+                    getResultTextView.text = "That didn't work!"
+                }
+            ))
         }
+
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
