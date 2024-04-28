bce16288d91c489f95270150f13aa2eb0d19ff38
Santiago Forero A <sanfo0620@gmail.com>
2024-04-28 14:58:46 -0500

    feature: refactor to musician list

--- app/src/main/java/com/vinyl/app/adapters/MusicianListAdapter.kt
+++ app/src/main/java/com/vinyl/app/adapters/MusicianListAdapter.kt
@@ -1,30 +1,36 @@
 package com.vinyl.app.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.vinyl.app.R
 import com.vinyl.app.databinding.MusicianListItemBinding
 import com.vinyl.app.pojo.Musician
 
-class MusicianListAdapter:RecyclerView.Adapter<MusicianListAdapter.MusicianListViewHolder>() {
+class MusicianListAdapter : RecyclerView.Adapter<MusicianListAdapter.MusicianListViewHolder>() {
 
-    lateinit var onItemClick:((Musician) -> Unit)
+    lateinit var onItemClick: ((Musician) -> Unit)
     private var musicianList = ArrayList<Musician>()
 
-    fun setMusicians(musicianList: ArrayList<Musician>){
+    fun setMusicians(musicianList: ArrayList<Musician>) {
         this.musicianList = musicianList
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MusicianListViewHolder {
-        return MusicianListViewHolder(MusicianListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_musician, parent, false)
+        return MusicianListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MusicianListViewHolder, position: Int) {
-        holder.binding.musicianNameTv.text = musicianList[position].name
-
-        holder.itemView.setOnClickListener{
-            onItemClick.invoke(musicianList[position])
+        val musician = musicianList[position]
+        holder.bind(musician)
+        holder.itemView.setOnClickListener {
+            onItemClick.invoke(musician)
         }
     }
 
@@ -32,8 +38,18 @@ class MusicianListAdapter:RecyclerView.Adapter<MusicianListAdapter.MusicianListV
         return musicianList.size
     }
 
+    inner class MusicianListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val musicianImage: ImageView = itemView.findViewById(R.id.musician_image)
+        private val musicianName: TextView = itemView.findViewById(R.id.musician_name)
+        private val musicianDescription: TextView = itemView.findViewById(R.id.musician_description)
 
-
-    class MusicianListViewHolder(var binding: MusicianListItemBinding):RecyclerView.ViewHolder(binding.root)
-
+        fun bind(musician: Musician) {
+            musicianName.text = musician.name
+            musicianDescription.text = musician.birthDate.split("-").first()
+            Glide.with(itemView.context)
+                .load(musician.image)
+                .circleCrop()
+                .into(musicianImage)
+        }
+    }
 }
--- app/src/main/java/com/vinyl/app/fragments/MusicianListFragment.kt
+++ app/src/main/java/com/vinyl/app/fragments/MusicianListFragment.kt
@@ -20,9 +20,9 @@ import com.vinyl.app.databinding.FragmentMusicianListBinding
 import com.vinyl.app.pojo.Album
 import com.vinyl.app.pojo.Musician
 import com.vinyl.app.viewmodel.MusicianViewModel
-
-
 class MusicianListFragment : Fragment() {
+
+
 
     private lateinit var binding: FragmentMusicianListBinding
     private lateinit var musicianMVVM: MusicianViewModel
@@ -81,78 +81,6 @@ class MusicianListFragment : Fragment() {
         }
     }
 
-    private fun loadMusicians() {
-
-        // Create an empty list to store the mock musicians
-        val mockMusicians = ArrayList<Musician>()
-
-        // Add some mock musicians to the list
-        mockMusicians.add(
-            Musician(
-                id = "1",
-                birthDate = "1992-08-19",
-                description = "Renowned Colombian singer and songwriter known for his smooth vocals and captivating melodies, blending reggaeton, Latin pop, and urban styles.",
-                image = "https://yt3.googleusercontent.com/GUW78kIdMM2mVjl-c1rkSD8DqNYSRZAfTUTie3j4xKFN6agTpdb9UiMDGwQB2yuoDpKB1a8QNn8=s900-c-k-c0x00ffffff-no-rj",
-                name = "Feid"
-            )
-        )
-        mockMusicians.add(
-            Musician(
-                id = "2",
-                birthDate = "1991-02-14",
-                description = "Chart-topping singer-songwriter celebrated for her empowering lyrics and vibrant stage presence, a leading figure in the Latin music industry.",
-                image = "https://upload.wikimedia.org/wikipedia/commons/c/cb/2023-11-16_Gala_de_los_Latin_Grammy%2C_15.jpg",
-                name = "Karol G"
-            )
-        )
-        mockMusicians.add(
-            Musician(
-                id = "3",
-                birthDate = "1990-08-19",
-                description = "Multi-talented artist known for his unique blend of Latin trap and reggaeton, pushing boundaries with his innovative sound.",
-                image = "https://yt3.googleusercontent.com/MH1BdZQE2X6k3WlIns2YlyBNude9Qh3nN0dnG-_zvkM1o5gzBS3upL-C2w6Xm6DysPyg4x8ZHhQ=s900-c-k-c0x00ffffff-no-rj",
-                name = "Bad Bunny"
-            )
-        )
-
-        mockMusicians.add(
-            Musician(
-                id = "4",
-                birthDate = "1999-06-27",
-                description = "Puerto Rican singer and songwriter known for his versatile style, blending reggaeton, trap, and R&B influences.",
-                image = "https://www.latercera.com/resizer/p_y3YQy7Np9-T8W9rpGXC8jrSdA=/900x600/smart/cloudfront-us-east-1.images.arcpublishing.com/copesa/E3YIDFMCBBCQNFJJXGLIE6WILI.jpg",
-                name = "Mora"
-            )
-        )
-
-        mockMusicians.add(
-            Musician(
-                id = "5",
-                birthDate = "1996-09-15",
-                description = "Emerging Latin artist celebrated for his energetic performances and dynamic rap flow, gaining recognition for his unique sound.",
-                image = "https://es.rollingstone.com/wp-content/uploads/2024/03/YOVNGCHIMI-y-la-nueva-generacion-del-drill-2.jpg",
-                name = "YOVNGCHIMI"
-            )
-        )
-
-        mockMusicians.add(
-            Musician(
-                id = "6",
-                birthDate = "1998-11-03",
-                description = "Talented rapper and singer known for his infectious charisma and compelling storytelling, making waves in the Latin music scene.",
-                image = "https://hips.hearstapps.com/hmg-prod/images/photo-feb-16-2024-7-49-10-pm-660f6efbe410b.jpg",
-                name = "Young Miko"
-            )
-        )
-
-
-
-
-
-
-
-        musicianListAdapter.setMusicians(mockMusicians)
-    }
 
     private fun prepareListRecyclerView() {
         binding.recViewMusicians.apply {
