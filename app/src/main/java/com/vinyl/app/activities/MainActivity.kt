6c3a96f6caef4d63529f83c378d9530efd6717dd
Jader Pelaez <j.pelaezg@uniandes.edu.co>
2024-05-25 17:18:23 -0500

    test no funcional

--- app/src/main/java/com/vinyl/app/activities/MainActivity.kt
+++ app/src/main/java/com/vinyl/app/activities/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.vinyl.app.activities
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.Navigation
@@ -8,11 +9,14 @@ import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.vinyl.app.R
 import com.vinyl.app.databinding.ActivityMainBinding
+import com.vinyl.app.viewmodel.HomeViewModel
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +26,15 @@ class MainActivity : AppCompatActivity() {
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main)
         NavigationUI.setupWithNavController(bottomNavigation,navController)
+        setupSwipeRefresh()
+    }
+    private fun setupSwipeRefresh() {
+        val swipeRefreshLayout = findViewById<SwipeRefreshLayout>(R.id.swipe_refresh)
+        swipeRefreshLayout.setOnRefreshListener {
+            homeViewModel.getAlbums()
+            swipeRefreshLayout.isRefreshing = false
+            Toast.makeText(this, "Page Refreshed!", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
